7015db7b87700bb883b1f0732a7de49fb46ecfab
Ali Briceño <alienriquebm@gmail.com>
2016-01-23 10:04:00 -0430

    + Agregado barra de carga + Correcion de errores de validacion

--- js/logincontroller.js
+++ js/logincontroller.js
@@ -1,22 +1,40 @@
 angular
-	.module('loginApp',['ngMessages'])
-	.controller('loginController', ['$http', function ($http) {
+	.module('loginApp',['ngMessages', 'angular-loading-bar'])
+	.controller('loginController', ['$http','$scope','$timeout', function ($http,$scope,$timeout) {
 		var scope = this;
 		scope.userName = "";
 		scope.password = "";
 		scope.respuesta = "";
+		scope.error = false;
 
 		scope.checkLogIn = function(){
-			alert("El usuario es " + scope.userName + " y su clave " + scope.password);
 			return $http({
-	            method: "GET",
-	            url: "http://192.168.1.134:53910/api/Account/UserInfo",
-	            headers: { 'Content-Type': 'application/json' }
+	            method: "POST",
+	            url: "http://192.168.0.203//testWebApi/Token",
+	            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+	            data: 'username=' + encodeURIComponent(scope.userName) + '&password=' + encodeURIComponent(scope.password) + '&grant_type=password'
 	        }).success(function (data) {
 	            scope.respuesta = data;
 	            console.log(data);
 	        }).error(function (data) {
 	            console.log(data);
+	            if (data.error === "invalid_grant" ) {
+					scope.error = true;
+					 $timeout(function(){
+						$scope.$broadcast('dataloaded');
+					});
+	            };
+	            
 	        });
 		};
-	}]);
+	}])
+	.directive('showPopup', function() {
+	  return {
+	    restrict: 'A',
+	    link: function(scope, element, attrs){
+	    	$scope.$on('dataloaded',function () {
+	    		alert("Hola");
+				})
+			}
+		};
+	});
