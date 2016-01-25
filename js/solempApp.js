184cb87ca86679687734ef3e4bbd18aeaaa57a24
Ali Briceño <alienriquebm@gmail.com>
2016-01-25 13:17:49 -0430

    Agregadas rutas de Menu de Hoteles, Compañias

--- js/solempApp.js
+++ js/solempApp.js
@@ -13,6 +13,10 @@ angular
            url: '/menuhotels',
             templateUrl: 'menuhotels.html'
         })
+		.state('menucompanies', {
+           url: '/menucompanies',
+            templateUrl: 'menucompanies.html'
+        })
         .state('logout', {
            url: '/logout',
             templateUrl: 'logout.html'
@@ -27,18 +31,23 @@ angular
   		.setStorageType('sessionStorage')
   		.setNotify(true, true);
 	})
+	.constant("solempWebApiURL", {
+        "url": "http://192.168.1.56/solempmobileWA/api",
+        "port": "80"
+    })
 	.controller('mainController', ['loginFactory','$scope','$rootScope',function(loginFactory,$scope,$rootScope){
 		var mc = this;
 		mc.isloggedIn = loginFactory.isLogged();
 		$rootScope.isloggedIn = loginFactory.isLogged();
+		$rootScope.actualPage = "main";
 	}])
-	.controller('loginController', ['$http','$scope','$location','$state','localStorageService','loginFactory','$rootScope', function ($http,$scope,$location,$state,localStorageService,loginFactory,$rootScope){
+	.controller('loginController', ['$http','$scope','$location','$state','localStorageService','loginFactory','$rootScope','solempWebApiURL', function ($http,$scope,$location,$state,localStorageService,loginFactory,$rootScope,solempWebApiURL){
 		var scope = this;
 		scope.userName = "";
 		scope.password = "";
 		scope.respuesta = "";
 		scope.error = false;
-
+		scope.onCheckLogin = false;
 		if (loginFactory.isLogged()) {
 			$state.go('menuhotels');
 		};
@@ -50,13 +59,14 @@ angular
 			};
 
 		scope.checkLogIn = function(){
-			console.log(scope.userName.toString());
-			console.log(scope.password.toString());
+			//Avisar que se está verificando el login
+			//para deshabilitar el botón de ingreso
+			scope.onCheckLogin = true;
 
 
 			return $http({
 	            method: "POST",
-	            url: "http://192.168.0.203/solempmobileWA/api/Users/getUser",
+	            url: solempWebApiURL.url + "/Users/getUser",
 	            headers: { 'Content-Type': 'application/json' },
 	            dataType: "json",
 	           	data: {
@@ -74,11 +84,22 @@ angular
 						scope.password = "";
 						//Reiniciar valores de validacion
 						$scope.logInfrm.$setPristine();
+						//habilitar botón de ingreso nuevamente
+						scope.onCheckLogin = false;
 	            	}
 	            } else {
+   					// escribo en el Session Storage la sesion y otros datos
    					localStorageService.set('userName', scope.userName);
    					localStorageService.set('loggedIn', 'yes');
+	            	// aviso que esta logueado el usuario
 	            	$rootScope.isloggedIn = true;
+					// Cargo los hoteles
+					localStorageService.set('hotels',data.Data[0].Hotels);
+					// Cargo las Compañías
+					localStorageService.set('companies',data.Data[0].Companies);
+					// Habilito boton de ingreso de nuevo
+					scope.onCheckLogin = false;
+					// Cargo menu de hoteles
 	            	$state.go('menuhotels');
 	            };
 	        }).error(function (data) {
@@ -89,6 +110,8 @@ angular
 					scope.password = "";
 					//Reiniciar valores de validacion
 					$scope.logInfrm.$setPristine();
+					// Habilito boton de ingreso de nuevo
+					scope.onCheckLogin = false;
 	            };
 	            
 	        });
@@ -96,12 +119,71 @@ angular
 	}])
 	.controller('logoutController', ['$scope','$rootScope','$state','localStorageService','loginFactory', function ($scope,$rootScope,$state,localStorageService,loginFactory){
 		console.log("Cerrando Session");
-		localStorageService.remove('loggedIn', 'userName');
+		localStorageService.clearAll();
 		$rootScope.isloggedIn = false;
+		//Aviso que estoy en la página main
+		$rootScope.actualPage = "main";
 		$state.go('solempmobile');
 	}])
 	.controller('hmenuController', ['$scope','$rootScope','$state','localStorageService','loginFactory', function ($scope,$rootScope,$state,localStorageService,loginFactory){
-		
+		hmc = this; 
+		hmc.hotels = localStorageService.get('hotels');
+		hmc.selectedHotel = "";
+		hmc.onSelectHotel = false;
+		//Aviso que estoy en la página de Hotels
+		$rootScope.actualPage = "hotels";
+		//Al seleccionar hotel:
+		hmc.hotelSelected = function() {
+			//Aviso que estoy procesando
+			//Deshabilito el boton para 
+			//evitar otro request
+			hmc.onSelectHotel = true;
+			// Guardo el hotel seleccionado
+			localStorageService.set('hotel', hmc.selectedHotel);
+			// Paso a seleccion de empresa
+			$state.go("menucompanies");
+			//Habilito de nuevo el botón
+			//Por si se regresa a Hoteles
+			hmc.onSelectHotel = false;
+		};
+	}])
+	.controller('cmenuController', ['$scope','$rootScope','$state','localStorageService','loginFactory', function ($scope,$rootScope,$state,localStorageService,loginFactory){
+		cmc = this; 
+		cmc.companies = localStorageService.get('companies');
+		cmc.selectedCompany = "";
+		cmc.onSelectCompany = false;
+		//Aviso que estoy en la página de Companies
+		$rootScope.actualPage = "companies";
+		//Al seleccionar hotel:
+		cmc.companySelected = function() {
+			//Aviso que estoy procesando
+			//Deshabilito el boton para 
+			//evitar otro request
+			cmc.onSelectCompany = true;
+			// Guardo el hotel seleccionado
+			localStorageService.set('company', cmc.selectedCompany);
+			// Paso al Menú Principal
+			
+			return $http({
+	            method: "POST",
+	            url: solempWebApiURL.url + "/Users/getDataForMainScreen",
+	            headers: { 'Content-Type': 'application/json' },
+	            dataType: "json",
+	           	data: {
+	           			"userName" : scope.userName,
+	           			"Password" : scope.password
+	           		}
+		        }).success(function (data) {
+		            
+		        }).error(function (data) {
+		            
+		        });
+			};
+
+			//Habilito de nuevo el botón
+			//Por si se regresa a Hoteles
+			cmc.onSelectCompany = false;
+		};
 	}])
 	.directive('showErrors', function() {
 	  return {
